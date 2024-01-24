5087dee595f29fe0c08fd59c2efca0b28bf2b0e3
lizhiyong <1679963578@qq.com>
2024-01-24 10:00:58 +0800

    del ad

--- ui/shared/ad/SliseBanner.tsx
+++ ui/shared/ad/SliseBanner.tsx
@@ -1,29 +1,7 @@
-import { Flex, chakra } from '@chakra-ui/react';
-import { SliseAd } from '@slise/embed-react';
-import React from 'react';
+import { chakra } from '@chakra-ui/react';
 
-import config from 'configs/app';
-
-const SliseBanner = ({ className }: { className?: string }) => {
-
-  return (
-    <>
-      <Flex className={ className } h="90px" display={{ base: 'none', lg: 'flex' }}>
-        <SliseAd
-          slotId={ config.chain.name || '' }
-          pub="pub-10"
-          format="728x90"
-          style={{ width: '728px', height: '90px' }}/>
-      </Flex>
-      <Flex className={ className } h="90px" display={{ base: 'flex', lg: 'none' }}>
-        <SliseAd
-          slotId={ config.chain.name || '' }
-          pub="pub-10"
-          format="270x90"
-          style={{ width: '270px', height: '90px' }}/>
-      </Flex>
-    </>
-  );
+const SliseBanner = () => {
+  return null;
 };
 
 export default chakra(SliseBanner);
